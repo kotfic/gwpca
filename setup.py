8dc480c4d8da6425724d5b8dd30eeac75717c643
Christopher Kotfila <chris.kotfila@kitware.com>
2017-11-03 11:02:16 -0400

    Add a long_description

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup, find_packages
 
+LONG_DESC="""
+This package implements a basic girder worker task plugin and is
+intended as a non-trivial example.
+
+It implements a single task that performs principal component analysis
+on a CSV with two components.  The task returns a dictionary with the
+explained variance of each component and a path to a figure that plots
+the data.  """
+
 setup(name='gwpca',
       version='0.1.0',
       description='An example girder worker extension',
+      long_description=LONG_DESC,
       author='Chris Kotfila',
       author_email='chris.kotfila@kitware.com',
       license='Apache v2',
       classifiers=[
           'Development Status :: 2 - Pre-Alpha',
-          'License :: OSI Approved :: Apache Software License'
-          'Topic :: Scientific/Engineering :: GIS',
+          'License :: OSI Approved :: Apache Software License',
+          'Topic :: Scientific/Engineering',
           'Intended Audience :: Science/Research',
           'Natural Language :: English',
           'Programming Language :: Python'
