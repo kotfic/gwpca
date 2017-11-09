21068b10fa57c3c5c884c4eb6a6c2fd4927bfa29
Christopher Kotfila <chris.kotfila@kitware.com>
2017-11-09 14:58:05 -0500

    Move transforms into girder_worker_utils

--- server/rest.py
+++ server/rest.py
@@ -5,17 +5,19 @@ from girder.constants import AccessType, TokenScope
 from girder.models.file import File as FileModel
 from girder.plugins.jobs.models.job import Job as JobModel
 
-from gwpca import GirderFileId, GirderItemMetadata, GirderUploadToItem
+from girder_worker_utils.transforms.girder_io import (
+    GirderFileId,
+    GirderItemMetadata,
+    GirderUploadToItem)
+
 from gwpca.tasks import gw_pca
-
-
 
 class PCA(Resource):
 
     def __init__(self):
         super(PCA, self).__init__()
         self.resourceName = 'pca'
-        self.route('GET', (), self.test)
+#        self.route('GET', (), self.test)
         self.route('POST', (':id',), self.generatePCAPlot)
 
     @access.public(scope=TokenScope.DATA_READ)
@@ -41,13 +43,13 @@ class PCA(Resource):
 
         return a.job
 
-    @filtermodel(model='job', plugin='jobs')
-    @autoDescribeRoute(
-        Description('Generate PCA for item with ID.'))
-    def test(self):
-        pass
-#        a = gw_pca.delay(GirderItemIdToDirectory(ITEMID))
-
-#        return a.job
-        # from common_tasks.test_tasks.fib import fibonacci
-        # fibonacci.delay(20)
+#     @filtermodel(model='job', plugin='jobs')
+#     @autoDescribeRoute(
+#         Description('Generate PCA for item with ID.'))
+#     def test(self):
+#         pass
+#         a = gw_pca.delay(GirderItemIdToDirectory(ITEMID))
+#
+#         return a.job
+#         # from common_tasks.test_tasks.fib import fibonacci
+#         # fibonacci.delay(20)
